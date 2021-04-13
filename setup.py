1065f84b001f7213ab9e8ca345866b8d94e6ac32
Will Dean <wdean@homepartners.com>
2021-04-13 12:58:16 -0500

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='repo_maker',
-    version='1.0',
+    version='1.1',
     author='William Dean',
     author_email='wdean@homepartners.com',
     description='Quickly create data science repo.',
