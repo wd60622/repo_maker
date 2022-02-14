b46c9daf9edd2faf12c31a8b3ff25c3bc721e742
Will Dean <57733339+wd60622@users.noreply.github.com>
2022-02-14 15:30:40 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 
 setup(
     name="repo_maker",
-    version="0.1.5",
+    version="0.1.6",
     author="William Dean",
-    author_email="wdean@homepartners.com",
+    author_email="wd60622@gmail.com",
     description="Quickly create data science repo.",
     packages=["repo_maker", "repo_maker/files"],
     scripts=["scripts/make_repo"],
