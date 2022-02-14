273a70c92009fe1ee405a206f46bae1cce89c596
Will Dean <57733339+wd60622@users.noreply.github.com>
2022-02-14 15:28:24 -0600

    Update utils.py

--- repo_maker/files/utils.py
+++ repo_maker/files/utils.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-DATA_DIR = Path(__file__).parent / "data"
+DATA_DIR = Path(__file__).parents[1] / "data"
 if not DATA_DIR.exists():
     DATA_DIR.mkdir()
     print("Creating data dir.")
